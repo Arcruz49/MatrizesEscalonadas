0fd4ad43f4a1bbf16124ac8777f629659d1e78db
Arthur Cruz <arthur.cruz@objetivasolucao.com.br>
2023-09-07 01:34:44 -0300

    atualizaçoes

--- MeuProjeto2/src/Projeto/Escalonamento.java
+++ MeuProjeto2/src/Projeto/Escalonamento.java
@@ -2,6 +2,11 @@ package Projeto;
 
 public class Escalonamento {
     //verifica se o pivo é igual a 1
+
+    public Matriz MatrizEscalonada(Matriz mat){
+
+        return mat;
+    }
     public boolean PivoIs1(Matriz mat, int i, int j){
         return mat.matriz[i][j] == 1;
     }
--- MeuProjeto2/src/Projeto/Principal.java
+++ MeuProjeto2/src/Projeto/Principal.java
@@ -1,6 +1,5 @@
 package Projeto;
 import java.util.Scanner;
-import Projeto.Escalonamento;
 
 public class Principal {
     public static void main(String[] args) throws Exception {
